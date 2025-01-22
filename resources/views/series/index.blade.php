5921fd8c511888a4fb7d1b850e5e621b9fc8d66f
felprangel <feliperangel897@gmail.com>
2025-01-21 22:31:46 -0300

    utiliza componente de layout

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Séries</title>
-</head>
-<body>
-    <h1>Séries</h1>
-
+<x-layout title="Séries">
     @foreach ($series as $serie)
-        <p>{{ $serie }}</p>
+    <p>{{ $serie }}</p>
     @endforeach
-</body>
-</html>
+</x-layout>

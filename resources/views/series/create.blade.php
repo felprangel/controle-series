562398b811a217bfc05968be1d7933a30d231f52
felprangel <feliperangel897@gmail.com>
2025-01-21 22:46:05 -0300

    adiciona form de adição de séries

---
+++ resources/views/series/create.blade.php
@@ -0,0 +1,7 @@
+<x-layout title="Nova Série">
+    <form method="post">
+        <label for="name">Nome da Série:</label>
+        <input type="text" name="name" id="name">
+        <button>Adicionar</button>
+    </form>
+</x-layout>

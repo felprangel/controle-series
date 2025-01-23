a522e1514c04ec69fb3b8cf9e73b729845f31b52
felprangel <feliperangel897@gmail.com>
2025-01-23 13:50:55 -0300

    adiciona metodo no resource

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,5 @@
 use App\Http\Controllers\SeriesController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('/series', SeriesController::class)->only(['index', 'create', 'store']);
-
-Route::post('/series/destroy/{id}', [SeriesController::class, 'destroy'])->name('series.destroy');
+Route::resource('/series', SeriesController::class)->only(['index', 'create', 'store', 'destroy']);
 

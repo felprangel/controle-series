e6516740632bcd8387ca9c9bfb8710d0282d37f3
felprangel <feliperangel897@gmail.com>
2025-01-22 22:57:43 -0300

    nomeia rota

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\SeriesController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(SeriesController::class)->group(function() {
-    Route::get('/series', 'index');
+    Route::get('/series', 'index')->name('series.index');
     Route::get('/series/criar', 'create')->name('series.create');
     Route::post('/series', 'store');
 });

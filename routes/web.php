1c9f29ec24ee8983d599b07687fee9ef94493652
felprangel <feliperangel897@gmail.com>
2025-01-22 21:27:32 -0300

    remove rota inicial do laravel

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,6 @@
 use App\Http\Controllers\SeriesController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/series', [SeriesController::class, 'index']);
 Route::get('/series/criar', [SeriesController::class, 'create']);
 Route::post('/series', [SeriesController::class, 'store']);

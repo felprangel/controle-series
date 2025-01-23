5c5dadd77eae587837ab712b37a1486970c27a8f
felprangel <feliperangel897@gmail.com>
2025-01-22 21:31:34 -0300

    adiciona tratativa para csrf

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,5 +1,6 @@
 <x-layout title="Nova Série">
     <form action="/series" method="post">
+        @csrf
         <label for="name">Nome da Série:</label>
         <input type="text" name="name" id="name">
         <button>Adicionar</button>

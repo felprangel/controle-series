0b88764ff530c62a10dd5f70c4f6952b4c50a0db
felprangel <feliperangel897@gmail.com>
2025-01-22 22:59:15 -0300

    altera forma de declarar as rotas

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use App\Http\Controllers\SeriesController;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(SeriesController::class)->group(function() {
-    Route::get('/series', 'index')->name('series.index');
-    Route::get('/series/criar', 'create')->name('series.create');
-    Route::post('/series', 'store');
-});
+Route::resource('/series', SeriesController::class);
 

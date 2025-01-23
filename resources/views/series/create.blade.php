10f0e5e74d0e97ae6969d9e51b145493cb2f0c33
felprangel <feliperangel897@gmail.com>
2025-01-23 15:41:03 -0300

    move exibição de erros pro layout

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,13 +1,4 @@
 <x-layout title="Nova Série">
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
     <form action={{ route('series.store') }} method="post">
         @csrf
         <label for="name">Nome da Série:</label>

86e6f71bc52a268e7bcd2c140a0f8c50d3415fc1
felprangel <feliperangel897@gmail.com>
2025-01-23 15:39:52 -0300

    adiciona exibição dos erros

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,4 +1,13 @@
 <x-layout title="Nova Série">
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
     <form action={{ route('series.store') }} method="post">
         @csrf
         <label for="name">Nome da Série:</label>

02d0eec53da4c49f6de41d1c7e8f46b64b02ade8
felprangel <feliperangel897@gmail.com>
2025-01-23 14:35:34 -0300

    altera funções do controller na rota de resource

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use App\Http\Controllers\SeriesController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('/series', SeriesController::class)->only(['index', 'create', 'store', 'destroy']);
+Route::resource('/series', SeriesController::class)->except(['show']);
 

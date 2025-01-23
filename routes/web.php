9058ff2e75be22bc2c99873feac9841f11054576
felprangel <feliperangel897@gmail.com>
2025-01-22 22:53:17 -0300

    altera organização das rotas

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use App\Http\Controllers\SeriesController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/series', [SeriesController::class, 'index']);
-Route::get('/series/criar', [SeriesController::class, 'create']);
-Route::post('/series', [SeriesController::class, 'store']);
+Route::controller(SeriesController::class)->group(function() {
+    Route::get('/series', 'index');
+    Route::get('/series/criar', 'create');
+    Route::post('/series', 'store');
+});
 

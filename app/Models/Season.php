23d685034279d5b0be7bc715e7005f122d43e4f4
felprangel <feliperangel897@gmail.com>
2025-01-26 19:52:47 -0300

    aidiciona relacionamento entre as models

--- app/Models/Season.php
+++ app/Models/Season.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Season extends Model
 {
-    //
+    public function series() {
+        return $this->belongsTo(Serie::class);
+    }
+
+    public function episodes() {
+        return $this->hasMany(Episode::class);
+    }
 }
--- app/Models/Serie.php
+++ app/Models/Serie.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Serie extends Model
 {
     protected $fillable = ['name'];
+
+    public function season() {
+        return $this->hasMany(Season::class);
+    }
 }

2218b87a6bd0037106b8361d4e6bae07994800d4
felprangel <feliperangel897@gmail.com>
2025-01-27 21:54:28 -0300

    adiciona inputs de temporada e episodios no formulario de criação

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -3,6 +3,10 @@
         @csrf
         <label for="name">Nome da Série:</label>
         <input type="text" name="name" id="name">
+        <label for="seasons">Número de Temporadas</label>
+        <input type="text" name="seasons" id="seasons">
+        <label for="episodes">Episódios por Temporada</label>
+        <input type="text" name="episodes" id="episodes">
         <button>Adicionar</button>
     </form>
 </x-layout>

44ce1162a9a5d83466e0f60194e3a7869cbe1be6
felprangel <feliperangel897@gmail.com>
2025-01-27 23:00:02 -0300

    adiciona rota para exibir as temporadas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\SeasonsController;
 use App\Http\Controllers\SeriesController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('/series', SeriesController::class)->except(['show']);
+
+Route::get('series/{series}/seasons', [SeasonsController::class, 'index'])->name('seasons.index');
 

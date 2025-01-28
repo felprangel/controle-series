7f087f805ac680f84de582cf8b7bf38ea0886c65
felprangel <feliperangel897@gmail.com>
2025-01-27 23:02:28 -0300

    adiciona função de exibição no controller

--- app/Http/Controllers/SeasonsController.php
+++ app/Http/Controllers/SeasonsController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Serie;
 use Illuminate\Http\Request;
 
 class SeasonsController extends Controller
 {
-    //
+    public function index(Serie $series)
+    {
+        $seasons = $series->seasons;
+
+        return view('seasons.index')->with('seasons', $seasons);
+    }
 }

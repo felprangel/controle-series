bab0c31d9b66c9028363f013bfd5fff8d21eb3d8
felprangel <feliperangel897@gmail.com>
2025-01-30 23:06:13 -0300

    adiciona rota post para atualizar os episodios

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,4 @@ Route::resource('/series', SeriesController::class)->except(['show']);
 Route::get('/series/{series}/seasons', [SeasonsController::class, 'index'])->name('seasons.index');
 
 Route::get('/seasons/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
+Route::post('/seasons/{season}/episodes', [EpisodesController::class, 'update'])->name('episodes.update');

1302439eb8e862ebc6c0cce09bef3cc74d59ea18
felprangel <feliperangel897@gmail.com>
2025-01-30 23:05:57 -0300

    adiciona função para atualizar os episodios como assistidos

--- app/Http/Controllers/EpisodesController.php
+++ app/Http/Controllers/EpisodesController.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Episode;
 use App\Models\Season;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Request;
 
 class EpisodesController extends Controller
 {
     public function index(Season $season)
     {
         return view('episodes.index')->with('episodes', $season->episodes);
+    }
+
+    public function update(Season $season)
+    {
+        $watchedEpisodes = Request::input('episodes');
+        $season->episodes->each(function (Episode $episode) use ($watchedEpisodes) {
+            $episode->watched = in_array($episode->id, $watchedEpisodes);
+        });
+
+        $season->push();
+
+        return Redirect::to(route('episodes.index', $season->id));
     }
 }

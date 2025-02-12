eb48e237a469908502696b2d36ed8b13dfed2e72
felprangel <feliperangel897@gmail.com>
2025-02-12 07:09:12 -0300

    adiciona campos a mais para registro

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,10 +1,14 @@
 <x-layout title="Novo Usuário" >
     <form method="post" action="{{ route('users.store') }}">
         @csrf
+        <label for="username">Email</label>
+        <input type="text" name="username" required >
         <label for="email">Email</label>
         <input type="email" name="email" required >
         <label for="password">Password</label>
         <input type="password" name="password" required >
+        <label for="confirm_password">Password</label>
+        <input type="password" name="confirm_password" required >
         <button type="submit">Entrar</button>
     </form>
 </x-layout>

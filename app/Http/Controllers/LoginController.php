dd6c36e4853baa9381ae46bc46d0639aae1ba754
felprangel <feliperangel897@gmail.com>
2025-02-11 21:15:22 -0300

    adiciona função para logar

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Request;
+
 class LoginController extends Controller
 {
     public function login()
     {
         return view('login.index');
+    }
+
+    public function store()
+    {
+        dd(Auth::attempt(Request::all()));
     }
 }

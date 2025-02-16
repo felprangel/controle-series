192e64d19bf91799e3886e3b02f9960c50f7492a
felprangel <feliperangel897@gmail.com>
2025-02-16 19:22:45 -0300

    remove inputs

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -7,8 +7,6 @@
         <input type="email" name="email" required >
         <label for="password">Password</label>
         <input type="password" name="password" required >
-        <label for="confirm_password">Password</label>
-        <input type="password" name="confirm_password" required >
         <button type="submit">Entrar</button>
     </form>
 </x-layout>

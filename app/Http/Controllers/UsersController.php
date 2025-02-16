81fdebcc3d76854d31816593de1ae14f191a4d1c
felprangel <feliperangel897@gmail.com>
2025-02-16 19:42:29 -0300

    adiciona lógica para criação de usuário

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Request;
+
 class UsersController extends Controller
 {
     public function create()
@@ -9,5 +14,13 @@ class UsersController extends Controller
         return view('users.create');
     }
 
-    public function store() {}
+    public function store()
+    {
+        $data = Request::except(['_token']);
+        $data['password'] = Hash::make($data['password']);
+        $user = User::create($data);
+        Auth::login($user);
+
+        return redirect()->route('series.index');
+    }
 }

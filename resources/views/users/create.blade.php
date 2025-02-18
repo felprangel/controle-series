6081a6ffdb6d5987eaaed2a2819089d4b73faade
felprangel <feliperangel897@gmail.com>
2025-02-17 21:57:03 -0300

    adiciona campo de nome no formulario de registro

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,8 +1,8 @@
 <x-layout title="Novo Usuário" >
     <form method="post" action="{{ route('users.store') }}">
         @csrf
-        <label for="username">Email</label>
-        <input type="text" name="username" required >
+        <label for="username">Name</label>
+        <input type="text" name="name" required >
         <label for="email">Email</label>
         <input type="email" name="email" required >
         <label for="password">Password</label>

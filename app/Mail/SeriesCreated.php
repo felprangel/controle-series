18bfd4c3636051affa6582813adf75d344fe6e9f
felprangel <feliperangel897@gmail.com>
2025-02-17 23:04:13 -0300

    adiciona propriedades no construtor

--- app/Mail/SeriesCreated.php
+++ app/Mail/SeriesCreated.php
@@ -16,7 +16,7 @@ class SeriesCreated extends Mailable
     /**
      * Create a new message instance.
      */
-    public function __construct()
+    public function __construct(public string $seriesName, public int $seriesId)
     {
         //
     }
